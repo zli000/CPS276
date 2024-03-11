41ec19fe55de5881986666e952319a1c1ece8be3
zli000 <zli3@wccnet.edu>
2024-03-10 23:18:19 -0400

    Assignment 5

---
+++ Assignments/Assignment 5/directories.php
@@ -0,0 +1,7 @@
+<?php
+class Directories{
+    public function create($filename){
+        
+    }
+}
+?>
--- Assignments/Assignment 5/site.php
+++ Assignments/Assignment 5/site.php
@@ -1,3 +1,7 @@
+<?php
+require_once "directories.php";
+?>
+
 <!doctype html>
 <html>
 <head>
